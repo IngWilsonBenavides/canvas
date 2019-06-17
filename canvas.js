798b357839d1b940c4cd9fe6cb8c12f50b3e9e1c
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-06-17 11:28:54 -0500

    ep 3. create a multiple circles with function and update

--- canvas.js
+++ canvas.js
@@ -35,46 +35,58 @@ var c = canvas.getContext('2d');
 // 	c.stroke();
 // }
 
-function Circle(x, y) {
+function Circle(x, y, dx, dy, radius) {
 	this.x = x;
 	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
 
 	this.draw = function () {
 		c.beginPath();
-		c.arc(x, y, 30, 0, Math.PI * 2, false);
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 		c.strokeStyle = 'blue';
 		c.stroke();
 	}
+
+	this.update = function() {
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+			this.dx = -this.dx;
+		}
+
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+			this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+		
+		this.draw();
+	}
 }
 
-var circle = new Circle(200, 200);
+var circleArray = [];
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
-var radius = 30;
+for (var i = 0; i < 100; i++) {
+
+	var radius = 30;
+	var x = Math.random() * (innerWidth - radius * 2) + radius;
+	var y = Math.random() * (innerHeight - radius * 2) + radius;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;	
+
+	circleArray.push(new Circle(x, y, dx, dy, radius));
+	var circle = new Circle(200, 200, 3, 3, 30);	
+}
+
 function animate() {
 	requestAnimationFrame(animate);
 	c.clearRect(0, 0, innerWidth, innerHeight);
 
-	circle.draw();
-
-	c.beginPath();
-	c.arc(x, y, 30, 0, Math.PI * 2, false);
-	c.strokeStyle = 'blue';
-	c.stroke();
-
-	if (x + radius > innerWidth || x - radius < 0) {
-		dx = -dx;
+	for (var i = 0; i < circleArray.length; i++) {
+		circleArray[i].update();
 	}
 
-	if (y + radius > innerHeight || y - radius < 0) {
-		dy = -dy;
-	}
-
-	x += dx;
-	y += dy;
 }
 
 animate();
