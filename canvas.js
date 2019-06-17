2ce869ad21adaec3d31e19674bee2c4b0faa975c
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-06-17 14:06:48 -0500

    ep 4. 16:01 los circulos siguen al mouse y cada uno tiene un color aleatorio escogido de un arreglo de colores

--- canvas.js
+++ canvas.js
@@ -40,6 +40,17 @@ var mouse = {
 	y: undefined
 }
 
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = [
+	'#ffaa33',
+	'#99ffaa',
+	'#00ff00',
+	'#4411aa',
+	'#ff1100',
+];
+
 window.addEventListener('mousemove', 
 	function(event) {
 		mouse.x = event.x;
@@ -57,8 +68,7 @@ function Circle(x, y, dx, dy, radius) {
 	this.draw = function () {
 		c.beginPath();
 		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.strokeStyle = 'blue';
-		c.stroke();
+		c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
 		c.fill();
 	}
 
@@ -76,8 +86,10 @@ function Circle(x, y, dx, dy, radius) {
 
 		//interactivity
 		if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-			this.radius += 1;
-		} else if (this.radius > 2) {
+			if (this.radius < maxRadius) {
+				this.radius += 1;
+			}
+		} else if (this.radius > minRadius) {
 			this.radius -= 1;
 		}
 		
