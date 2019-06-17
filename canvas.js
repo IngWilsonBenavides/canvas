d4fc9134d609f666ed9bf5b8f3b6efe0cfe0835b
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-06-17 08:22:06 -0500

    ep 3. 04:57 draw screen on x axis with animate function

--- canvas.js
+++ canvas.js
@@ -4,21 +4,21 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(300, 300, 100, 100);
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(300, 300, 100, 100);
 
-console.log(canvas);
+//console.log(canvas);
 // Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa34a3";
-c.stroke();
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
 
 //Arc / Circle
 //c.beginPath();
@@ -26,11 +26,26 @@ c.stroke();
 //c.strokeStyle = 'blue';
 //c.stroke();
 
-for (var i = 0; i < 100; i++) {
-	var x = Math.random() * window.innerWidth;
-	var y = Math.random() * window.innerHeight;
+// for (var i = 0; i < 100; i++) {
+// 	var x = Math.random() * window.innerWidth;
+// 	var y = Math.random() * window.innerHeight;
+// 	c.beginPath();
+// 	c.arc(x, y, 30, 0, Math.PI * 2, false);
+// 	c.strokeStyle = 'blue';
+// 	c.stroke();
+// }
+
+var x = 200;
+function animate() {
+	requestAnimationFrame(animate);
+
 	c.beginPath();
-	c.arc(x, y, 30, 0, Math.PI * 2, false);
+	c.arc(x, 200, 30, 0, Math.PI * 2, false);
 	c.strokeStyle = 'blue';
 	c.stroke();
+
+	x += 1;
 }
+
+animate();
+
